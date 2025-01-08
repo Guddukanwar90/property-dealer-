35ad0a18c5875306be99405e540da0398e3148ef
Guddu Kanwar <157600619+Guddukanwar90@users.noreply.github.com>
2025-01-08 19:37:18 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import Footer from './pages/Footer.js';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <Footer/> */}
+  </React.StrictMode>
+);
+
+
